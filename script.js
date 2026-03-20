e5a24979ac3a2b6737601372e5da8acc31374025
THen25 <192696346+THen25@users.noreply.github.com>
2026-03-20 19:10:29 -0400

    Final edit

--- script.js
+++ script.js
@@ -21,40 +21,6 @@ const getHumanChoice = () => {
   return userChoice;
 };
 
-// Play a single round
-// const playRound = (humanChoice, computerChoice) => {
-//   humanChoice = humanChoice.toLowerCase();
-//   if (humanChoice === computerChoice) {
-//     console.log("Draw!");
-//   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-//     console.log(`You win! Rock beats Scissors`);
-//     humanScore++;
-//   } else if (humanChoice === "paper" && computerChoice === "rock") {
-//     console.log(`You win! Paper beats Rock`);
-//     humanScore++;
-//   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-//     console.log(`You win! Scissors beats Paper`);
-//     humanScore++;
-//   } else if (humanChoice === "rock" && computerChoice === "paper") {
-//     console.log(`You lose! Paper beats Rock`);
-//     computerScore++;
-//   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-//     console.log(`You lose! Scissors beats Paper`);
-//     computerScore++;
-//   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-//     console.log(`You lose! Rock beats Scissors`);
-//     computerScore++;
-//   }
-//   console.log(`Round results:
-//     Human Score: ${humanScore}
-//     Computer Score: ${computerScore}`);
-// };
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
-
 const playGame = () => {
   const playRound = (humanChoice, computerChoice) => {
     humanChoice = humanChoice.toLowerCase();
@@ -84,6 +50,7 @@ const playGame = () => {
     Computer Score: ${computerScore}`);
   };
 
+  //   Calls playRound() 5 times
   for (let i = 1; i <= 5; i++) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
